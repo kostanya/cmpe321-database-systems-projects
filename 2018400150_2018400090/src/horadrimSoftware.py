35e57febb6d194de5e5b02543518f538a529c6c1
haticerk <74210334+haticerk@users.noreply.github.com>
2022-06-02 22:28:53 +0300

    2.6,

--- 2018400150_2018400090/src/horadrimSoftware.py
+++ 2018400150_2018400090/src/horadrimSoftware.py
@@ -2,6 +2,7 @@ import os
 import math
 import csv
 import json
+import sys
 from bplustree import *
 from sessions import *
 from manipulation import *
@@ -27,7 +28,9 @@ bufferManager(types)
 # loading trees from previous sessions
 loadTrees(btrees, types)
 
-parsing("src/input.txt", "output.txt", types, btrees)
+input = sys.argv[1]
+output = sys.argv[2]
+parsing(input, output, types, btrees)
 
 # saving trees for next sessions
 saveTrees(btrees)
